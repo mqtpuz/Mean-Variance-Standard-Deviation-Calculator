d18cba78564e478e3c3f9e786a8f500dbdeb4624
Joaquim Schuler <qschuler@uol.com.br>
2024-05-22 15:16:33 +0000

    chore: completed freecodecamp course challenge

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,17 @@
 import numpy as np
 
-def calculate(list):
 
+def calculate(input_list):
+    if len(input_list) < 9:
+        raise ValueError('List must contain nine numbers.')
 
+    a = np.array(input_list).reshape((3, 3))
+    numpy_functions = [np.mean, np.var, np.std, np.max, np.min, np.sum]
+    calculations = {'mean': [], 'variance': [], 'standard deviation': [], 'max': [], 'min': [], 'sum': []}
 
+    for func, key in zip(numpy_functions, calculations):
+        calculations[key].append(list(func(a, axis=0)))
+        calculations[key].append(list(func(a, axis=1)))
+        calculations[key].append(func(a, axis=None))
 
     return calculations
